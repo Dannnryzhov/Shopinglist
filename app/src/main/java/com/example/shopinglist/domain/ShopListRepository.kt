8b6e42bee2760eba41600ecd029cae865267b450
d.n_ry <danlryzhov@gmail.com>
2024-11-07 17:25:59 +0300

    domain and data

---
+++ app/src/main/java/com/example/shopinglist/domain/ShopListRepository.kt
@@ -0,0 +1,9 @@
+package com.example.shopinglist.domain
+
+interface ShopListRepository {
+    fun addShopItem(shopItem: ShopItem)
+    fun getShopList(): List<ShopItem>
+    fun getShopItem(shopItemId: Int): ShopItem
+    fun editShopItem(shopItem: ShopItem)
+    fun deleteShopItem(shopItem: ShopItem)
+}
